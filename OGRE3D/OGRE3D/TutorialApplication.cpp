dbd5422896f4087133cbe9ee734e3ad183a8f108
pspkzar <cesar.perdigao92@gmail.com>
2013-09-26 16:42:20 +0100

    Basic Tutorial 1

--- OGRE3D/OGRE3D/TutorialApplication.cpp
+++ OGRE3D/OGRE3D/TutorialApplication.cpp
@@ -29,6 +29,31 @@ TutorialApplication::~TutorialApplication(void)
 void TutorialApplication::createScene(void)
 {
     // create your scene here :)
+
+	// Set the scene's ambient light
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5f, 0.5f, 0.5f));
+ 
+    Ogre::Entity* ogreHead = mSceneMgr->createEntity("Head", "ogrehead.mesh");
+	Ogre::SceneNode* headNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("HeadNode");
+	headNode->attachObject(ogreHead);
+	 
+	headNode->yaw( Ogre::Degree( -90 ) );
+ 
+	Ogre::Entity* ogreHead2 = mSceneMgr->createEntity( "Head2", "ogrehead.mesh" );
+	Ogre::SceneNode* headNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode( "HeadNode2", Ogre::Vector3( 100, 0, 0 ) );
+	headNode2->attachObject( ogreHead2 );
+ 
+	headNode2->pitch( Ogre::Degree( -90 ) );
+ 
+	Ogre::Entity* ogreHead3 = mSceneMgr->createEntity( "Head3", "ogrehead.mesh" );
+	Ogre::SceneNode* headNode3 = mSceneMgr->getRootSceneNode()->createChildSceneNode( "HeadNode3", Ogre::Vector3( 200, 0, 0 ) );
+	headNode3->attachObject( ogreHead3 );
+ 
+	headNode3->roll( Ogre::Degree( -90 ) );
+
+    // Create a Light and set its position
+    Ogre::Light* light = mSceneMgr->createLight("MainLight");
+    light->setPosition(20.0f, 80.0f, 50.0f);
 }
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
