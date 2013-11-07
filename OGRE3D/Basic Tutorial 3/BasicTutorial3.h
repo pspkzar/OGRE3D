17208425f7525c5072d911b47c2ddf9ac21d6d7b
pspkzar <cesar.perdigao92@gmail.com>
2013-11-07 10:20:50 +0000

    Tutorial 3

---
+++ OGRE3D/Basic Tutorial 3/BasicTutorial3.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "baseapplication.h"
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
+
+class BasicTutorial3 :
+	public BaseApplication
+{
+private:
+    Ogre::TerrainGlobalOptions* mTerrainGlobals;
+    Ogre::TerrainGroup* mTerrainGroup;
+    bool mTerrainsImported;
+	OgreBites::Label* mInfoLabel;
+ 
+    void defineTerrain(long x, long y);
+    void initBlendMaps(Ogre::Terrain* terrain);
+    void configureTerrainDefaults(Ogre::Light* light);
+
+public:
+	BasicTutorial3(void);
+	virtual ~BasicTutorial3(void);
+
+protected:
+	virtual void createScene(void);
+    virtual void createFrameListener(void);
+    virtual void destroyScene(void);
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+};
+
